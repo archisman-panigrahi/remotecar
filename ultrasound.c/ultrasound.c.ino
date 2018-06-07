48b69205f81e3c645aef83a7cf4f072583e2d9ff
Archisman Panigrahi <apandada1@gmail.com>
2018-06-07 12:25:19 +0530

    Let's use functions

--- ultrasound.c/ultrasound.c.ino
+++ ultrasound.c/ultrasound.c.ino
@@ -1,103 +1,58 @@
 /* HC-SR04 Sensor based on
    https://www.dealextreme.com/p/hc-sr04-ultrasonic-sensor-distance-measuring-module-133696
-  
-   This sketch reads a HC-SR04 ultrasonic rangefinder and returns the
-   distance to the closest object in range. To do this, it sends a pulse
-   to the sensor to initiate a reading, then listens for a pulse 
-   to return.  The length of the returning pulse is proportional to 
-   the distance of the object from the sensor.
+   and https://www.theengineeringprojects.com/2015/02/interfacing-multiple-ultrasonic-sensor-arduino.html
    
    The circuit:
   * VCC connection of the sensor attached to +5V
   * GND connection of the sensor attached to ground
-  * TRIG connection of the sensor attached to digital pin 2
+  * TRIG connection of the sensor attached to digital pin 2 (We are using a single trig pin for all sensors)
   * ECHO connection of the sensor attached to digital pin 4
-   Original code for Ping))) example was created by David A. Mellis
-   Adapted for HC-SR04 by Tautvidas Sipavicius
-   This example code is in the public domain.
- */
+
+*/
 
 
 const int trigPin = 2;
 
-const int echoPinLeft = 5;
-const int echoPinMid = 3;
+const int echoPinLeft = 3;
+const int echoPinMid = 5;
 const int echoPinRight = 6;
 
 const int LedLeft = 10;
 const int LedMid = 11;
 const int LedRight = 12;
 
-
+long distance;
+long duration;
 
 void setup() {
   // initialize serial communication:
   Serial.begin(9600);
-}
-
-void loop()
-{
-  // establish variables for duration of the ping, 
-  // and the distance result in inches and centimeters:
-  long durationLeft, durationMid, durationRight;
-  long cmLeft, cmMid, cmRight;
-
-  //Initialize LED and turn them LOW
-  pinMode(LedLeft, OUTPUT);
-  pinMode(LedMid, OUTPUT);
-  pinMode(LedRight, OUTPUT);
-
+  //Initialize LED
+  //pinMode(LedLeft, OUTPUT);
+  //pinMode(LedMid, OUTPUT);
+  //pinMode(LedRight, OUTPUT);
   //Input echo pins
   pinMode(echoPinLeft, INPUT);
   pinMode(echoPinMid, INPUT);
   pinMode(echoPinRight, INPUT);
+  //Output trig pin
+  pinMode(trigPin, OUTPUT);
+}
+
+void loop()
+{
+
+  long cmLeft, cmMid, cmRight;
 
   //digitalWrite(LedLeft, LOW);
   //digitalWrite(LedMid, LOW);
   //digitalWrite(LedRight, LOW);
-
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  pinMode(trigPin, OUTPUT);
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-
-  durationLeft = pulseIn(echoPinLeft, HIGH);
-
-  //Trigger again for middle pin
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  //Read signal for middle pin
-  durationMid = pulseIn(echoPinMid, HIGH);
-  
-  //Trigger again for right pin
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  
-  //Read signal for middle pin
-  durationRight = pulseIn(echoPinRight, HIGH);
-
-  // convert the time into a distance
-  //inches = microsecondsToInches(duration);
-  cmLeft = microsecondsToCentimeters(durationLeft);
-  cmMid = microsecondsToCentimeters(durationMid);
-  cmRight = microsecondsToCentimeters(durationRight);
-  
-
-  //Serial.print(inches);
-  //Serial.print("in, ");
-  
+  calculateDistance(trigPin, echoPinLeft);
+  cmLeft = distance;
+  calculateDistance(trigPin, echoPinMid);
+  cmMid = distance;
+  calculateDistance(trigPin, echoPinRight);
+  cmRight = distance;
   Serial.print(cmLeft);
   Serial.print("cm Left ");
 
@@ -105,9 +60,8 @@ void loop()
   Serial.print("cm Mid  ");
 
   Serial.print(cmRight);
-  Serial.print("cm Right");
-  Serial.println();
-  Serial.println();
+  Serial.println("cm Right");
+
   /*
   if (cmLeft <= 10)
   {
@@ -124,21 +78,17 @@ void loop()
   delay(10);
 }
 
-/*long microsecondsToInches(long microseconds)
-{
-  // According to Parallax's datasheet for the PING))), there are
-  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
-  // second).  This gives the distance travelled by the ping, outbound
-  // and return, so we divide by 2 to get the distance of the obstacle.
-  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
-  return microseconds / 74 / 2;
-}*/
 
-long microsecondsToCentimeters(long microseconds)
+void calculateDistance(int trigPin, int echoPin)
 {
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the
-  // object we take half of the distance travelled.
-  return microseconds / 29 / 2;
+
+	digitalWrite(trigPin, LOW);
+	delayMicroseconds(2);  //This is for a clear, high pulse
+	digitalWrite(trigPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trigPin, LOW);
+	duration = pulseIn(echoPin, HIGH);
+	//Sound travels 1 cm at 29.4 microseconds, it is 
+	//divided by 2 as the ultrasound travels out and back
+	distance = (duration/2)/29.4; 
 }
-
