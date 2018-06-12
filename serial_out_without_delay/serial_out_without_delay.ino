a8efd5cf5ed2f1897fa98d73f1b399ac40e10e16
Archisman Panigrahi <apandada1@gmail.com>
2018-06-12 10:51:14 +0530

    The array is now working as initially intended

--- serial_out_without_delay/serial_out_without_delay.ino
+++ serial_out_without_delay/serial_out_without_delay.ino
@@ -4,12 +4,12 @@ const int enable = 7;
 int i=0;
 int p=0; //This is for stopping the racing of clock
 
-int a[8] = {LOW,HIGH,LOW,HIGH,LOW,LOW,HIGH,LOW};//{Dummy,Dummy,Dummy,front left,front right,back left,back right,Dummy} 
+int a[8];// = {LOW,HIGH,LOW,HIGH,LOW,LOW,HIGH,HIGH};//{Dummy,Dummy,Dummy,Dummy,front left,front right,back left,back right} 
 //Motion of wheels - LOW is backward, HIGH is forward
-int forward[8] = {LOW,LOW,LOW,HIGH,HIGH,HIGH,HIGH,LOW};
+int forward[8] = {LOW,LOW,LOW,LOW,HIGH,HIGH,HIGH,HIGH};
 int backward[8] = {LOW,LOW,LOW,LOW,LOW,LOW,LOW};
-int right[8] = {LOW,LOW,LOW,HIGH,LOW,HIGH,LOW,LOW};
-int left[8] = {LOW,LOW,LOW,LOW,HIGH,LOW,HIGH,LOW};
+int right[8] = {LOW,LOW,LOW,LOW,HIGH,LOW,HIGH,LOW};
+int left[8] = {LOW,LOW,LOW,LOW,LOW,HIGH,LOW,HIGH};
 unsigned long previousTime = 0; //time of previous change
 unsigned long currentTime;
 long interval = 10; //time period of clock
@@ -20,8 +20,8 @@ void setup()
   pinMode(serial_out, OUTPUT);
   pinMode(clk, OUTPUT);
   pinMode(enable, OUTPUT);
+  copy(a,left,8); //This is temporary. Later we will change this dynamically with Raspberry Pi
   digitalWrite(clk, HIGH); //Initially clock should be high due to positive edge triggered IC
-  //copy(a,backward,8); //This is temporary. Later we will change this dynamically with Raspberry Pi
 }
 
 void alterValue (int pin)
@@ -42,8 +42,12 @@ void loop()
   {
   	if (i == 0)
   	{
-  		digitalWrite(enable, LOW);
+  		digitalWrite(enable, HIGH);
   	}
+    if (i == 1)
+    {
+      digitalWrite(enable, LOW);
+    }
     previousTime = currentTime;
   	digitalWrite(serial_out, a[i]);
     Serial.print(i);
@@ -52,7 +56,6 @@ void loop()
   	i++;
   	if(i == 8)
   	{
-  		digitalWrite(enable, HIGH);
   		i = 0;
   	}
     alterValue(clk);
