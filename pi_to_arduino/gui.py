dbfa211ea74ab1fe57bc9b21e85a3482b79aae83
Archisman Panigrahi <apandada1@gmail.com>
2018-06-14 15:08:20 +0530

    Get speed back after stop when the button is pressed

--- pi_to_arduino/gui.py
+++ pi_to_arduino/gui.py
@@ -1,5 +1,13 @@
 import serial
 try:
+        import RPi.GPIO as GPIO
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(3,GPIO.OUT)
+        p = GPIO.PWM(3,100)
+        p.start(40)
+except ImportError:
+        print ("Cannot import GPIO. Are you on Raspi?")
+try:
 	import tkinter as tk
 	from tkinter import *
 except ImportError:
@@ -12,18 +20,31 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 
 def forward():
 	ser.write("a")
+	if (w1.get() == 0):
+            w1.set(20)
+            speed(20)
 	print ("forward")
 def backward():
 	ser.write("b")
+	if (w1.get() == 0):
+            w1.set(20)
+            speed(20)
 	print ("backward")
 def left():
 	ser.write("c")
+	if (w1.get() == 0):
+            w1.set(20)
+            speed(20)
 	print ("left")
 def right():
 	ser.write("d")
+	if (w1.get() == 0):
+            w1.set(20)
+            speed(20)
 	print ("right")
 def speed(val):
 	print (val)
+        p.start(float(val))
 def stop():
 	speed(0)
 	w1.set(0)
