1933ccda4cc0a4af83433c75635dc3cbc92542f0
Archisman Panigrahi <apandada1@gmail.com>
2018-06-14 11:58:52 +0530

    No need to give input repeatedly

--- pi_to_arduino/motor.py
+++ pi_to_arduino/motor.py
@@ -2,31 +2,31 @@ import serial
 import time
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
-timeout = 0.5
+#timeout = 0.5
 
 while 1: 
 
-	start = time.time()
-	while (time.time() - start < timeout):
-		ser.write("a")
+	#start = time.time()
+	#while (time.time() - start < timeout):
+	ser.write("a")
 	print "forward"
 	#print (ser.readline())
 	time.sleep(5)
 	start = time.time()
-	while (time.time() - start < timeout):
-		ser.write("b")
+	#while (time.time() - start < timeout):
+	ser.write("b")
 	print "backward"
 	#print (ser.readline())
 	time.sleep(5)
 	start = time.time()
-	while (time.time() - start < timeout):
-		ser.write("c")
+	#while (time.time() - start < timeout):
+	ser.write("c")
 	print "left"
 	#print (ser.readline())
 	time.sleep(5)
 	start = time.time()
-	while (time.time() - start < timeout):
-		ser.write("d")
+	#while (time.time() - start < timeout):
+	ser.write("d")
 	print "right"
 	#print (ser.readline())
 	time.sleep(5)
