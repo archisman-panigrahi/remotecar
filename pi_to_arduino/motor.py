867123eee7ae771dc6953e95ec8c5cfeebbea9ee
Archisman Panigrahi <apandada1@gmail.com>
2018-06-14 11:48:30 +0530

    Now motor.py works - Need to implement GUI

--- pi_to_arduino/motor.py
+++ pi_to_arduino/motor.py
@@ -1,7 +1,32 @@
 import serial
-ser_input = serial.Serial('/dev/ttyACM0', 9600)
+import time
+
+ser = serial.Serial('/dev/ttyACM0', 9600)
+timeout = 0.5
 
 while 1: 
-	if (ser_input.inWaiting()>0):
-		data = ser_input.readline()
-		print data
+
+	start = time.time()
+	while (time.time() - start < timeout):
+		ser.write("a")
+	print "forward"
+	#print (ser.readline())
+	time.sleep(5)
+	start = time.time()
+	while (time.time() - start < timeout):
+		ser.write("b")
+	print "backward"
+	#print (ser.readline())
+	time.sleep(5)
+	start = time.time()
+	while (time.time() - start < timeout):
+		ser.write("c")
+	print "left"
+	#print (ser.readline())
+	time.sleep(5)
+	start = time.time()
+	while (time.time() - start < timeout):
+		ser.write("d")
+	print "right"
+	#print (ser.readline())
+	time.sleep(5)
